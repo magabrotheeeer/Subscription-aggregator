d3056b97f96460fa303e9ee7794a018795a4e312
Magomed <akhilgov_magomed@inbox.ru>
2025-07-24 11:22:04 +0300

    добавлен логгер

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/magabrotheeeer/subscription-aggregator/internal/config"
 )
 
 func main() {
 	config := config.MustLoad()
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	logger.Info("starting subscription-aggregator", slog.String("env", config.Env))
+	logger.Debug("debug messages are enabled")
 
 }
