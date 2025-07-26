a6a0d7dc0eed1d6f38f62666ba14e6f0a5318931
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-07-26 12:12:46 +0300

    хэндлер для создания новой записи пользователя

--- internal/http-server/handlers/create/create.go
+++ internal/http-server/handlers/create/create.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"log/slog"
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
-	"github.com/magabrotheeeer/subscription-aggregator/internal/subscription"
+	"github.com/go-playground/validator"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/http-server/response"
+	subs "github.com/magabrotheeeer/subscription-aggregator/internal/subscription"
 )
 
 type Creater interface {
-	CreateSubscriptionEntry(ctx context.Context, serviceName string, price int,
-		userID string, startDate time.Time, endDate time.Time) (int, error)
+	CreateSubscriptionEntry(ctx context.Context, entry subs.SubscriptionEntry) (int, error)
 }
 
-func New(log *slog.Logger, creater Creater) http.HandlerFunc {
+func New(ctx context.Context, log *slog.Logger, creater Creater) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const op = "handlers.create.New"
 
@@ -25,19 +25,42 @@ func New(log *slog.Logger, creater Creater) http.HandlerFunc {
 			slog.String("requires_id", middleware.GetReqID(r.Context())),
 		)
 
-		var req subscription.SubscriptionEntry
+		var req subs.SubscriptionEntry
 
 		err := render.DecodeJSON(r.Body, &req)
 		if err != nil {
-			log.Error("failed to decode request body", slog.Attr{Key: "err", Value: slog.StringValue(err.Error())})
+			log.Error("failed to decode request body", slog.Attr{
+				Key:   "err",
+				Value: slog.StringValue(err.Error())})
 
-			render.JSON(w, r, map[string]string{
-				"error":   "Invalid request body",
-				"details": err.Error(),
-			})
+			render.JSON(w, r, response.Error("failed to decode request"))
+
 			return
 		}
 		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+			log.Error("Invalid request", slog.Attr{
+				Key:   "err",
+				Value: slog.StringValue(err.Error()),
+			})
+
+			render.JSON(w, r, response.Error("Invalid request"))
+			render.JSON(w, r, response.ValidationError(validateErr))
+			return
+		}
+		_, err = creater.CreateSubscriptionEntry(ctx, req)
+		if err != nil {
+			log.Error("failed to create new entry", slog.Attr{
+				Key:   "err",
+				Value: slog.StringValue(err.Error()),
+			})
+			render.JSON(w, r, response.Error("failed to save"))
+			return
+		}
+		log.Info("created new entry")
+		render.JSON(w, r, response.StatusOK)
 
 	}
 }
