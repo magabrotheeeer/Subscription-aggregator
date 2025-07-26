a125ecd7b4fa55eacc4f30198591546d2ada9a82
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-07-26 12:10:01 +0300

    добавил структуру, которая хранит в себе данные о записи с подпиской

--- internal/subscription/subscription.go
+++ internal/subscription/subscription.go
@@ -1,11 +1,13 @@
 package subscription
 
-import "time"
+import (
+	"time"
+)
 
 type SubscriptionEntry struct {
-	ServiceName string    `json:"service_name"`
-	Price       int       `json:"price"`
-	UserID      string    `json:"user_id"`
-	StartDate   time.Time `json:"start_date"`
-	EndDate     *time.Time `json:"end_date,omitempty"`
+	ServiceName string     `json:"service_name" validator:"required,alphanum"`
+	Price       int        `json:"price" validator:"required,numeric"`
+	UserID      string     `json:"user_id" validator:"required,uuid"`
+	StartDate   time.Time  `json:"start_date" validator:"required,datetime=2006-01"`
+	EndDate     *time.Time `json:"end_date,omitempty" validator:"omitempty,datetime=2006-01"`
 }
