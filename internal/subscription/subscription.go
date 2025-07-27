5d91a008d96247b515e510e7d8ce5eead5591ea5
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-07-27 22:38:18 +0300

    создана структура DummySubscriptionEntry для конвертации в time.Time из json

--- internal/subscription/subscription.go
+++ internal/subscription/subscription.go
@@ -1,6 +1,16 @@
 package subscription
 
+import "time"
+
 type SubscriptionEntry struct {
+	ServiceName string     `json:"service_name" validator:"required,alphanum"`
+	Price       int        `json:"price" validator:"required,numeric"`
+	UserID      string     `json:"user_id" validator:"required,uuid"`
+	StartDate   time.Time  `json:"start_date" validator:"required,datetime=01-2006"`
+	EndDate     *time.Time `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
+}
+
+type DummySubscriptionEntry struct {
 	ServiceName string `json:"service_name" validator:"required,alphanum"`
 	Price       int    `json:"price" validator:"required,numeric"`
 	UserID      string `json:"user_id" validator:"required,uuid"`
