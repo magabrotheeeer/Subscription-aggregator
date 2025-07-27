7fc6dadee3fb706d2fa4e72ce05016ce287283a6
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-07-27 22:20:11 +0300

    поправил структурные теги

--- internal/subscription/subscription.go
+++ internal/subscription/subscription.go
@@ -8,6 +8,6 @@ type SubscriptionEntry struct {
 	ServiceName string     `json:"service_name" validator:"required,alphanum"`
 	Price       int        `json:"price" validator:"required,numeric"`
 	UserID      string     `json:"user_id" validator:"required,uuid"`
-	StartDate   time.Time  `json:"start_date" validator:"required,datetime=2006-01"`
-	EndDate     *time.Time `json:"end_date,omitempty" validator:"omitempty,datetime=2006-01"`
+	StartDate   time.Time  `json:"start_date" validator:"required,datetime=01-2006"`
+	EndDate     *time.Time `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
 }
