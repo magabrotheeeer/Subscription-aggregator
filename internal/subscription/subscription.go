24bd9c5cf54dc745195578ac29bd9a239cebf6e2
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-07-27 22:24:23 +0300

    поменял тип у дат

--- internal/subscription/subscription.go
+++ internal/subscription/subscription.go
@@ -1,13 +1,9 @@
 package subscription
 
-import (
-	"time"
-)
-
 type SubscriptionEntry struct {
-	ServiceName string     `json:"service_name" validator:"required,alphanum"`
-	Price       int        `json:"price" validator:"required,numeric"`
-	UserID      string     `json:"user_id" validator:"required,uuid"`
-	StartDate   time.Time  `json:"start_date" validator:"required,datetime=01-2006"`
-	EndDate     *time.Time `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
+	ServiceName string `json:"service_name" validator:"required,alphanum"`
+	Price       int    `json:"price" validator:"required,numeric"`
+	UserID      string `json:"user_id" validator:"required,uuid"`
+	StartDate   string `json:"start_date" validator:"required,datetime=01-2006"`
+	EndDate     string `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
 }
