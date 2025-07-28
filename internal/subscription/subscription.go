6fea886f2beb26d55782ecfd356162d646db4d16
Magomed <akhilgov_magomed@inbox.ru>
2025-07-28 13:38:36 +0300

    добавлены структуры для CRUDL-методов

--- internal/subscription/subscription.go
+++ internal/subscription/subscription.go
@@ -2,7 +2,8 @@ package subscription
 
 import "time"
 
-type CreateSubscriptionEntry struct {
+// Структура для хранения данных при первичном обращении к серверу
+type CreaterSubscriptionEntry struct {
 	ServiceName string     `json:"service_name" validator:"required,alphanum"`
 	Price       int        `json:"price" validator:"required,numeric"`
 	UserID      string     `json:"user_id" validator:"required,uuid"`
@@ -10,7 +11,8 @@ type CreateSubscriptionEntry struct {
 	EndDate     *time.Time `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
 }
 
-type DummySubscriptionEntry struct {
+// Структура для хранения данных, полученных в формате JSON
+type DummyCreaterSubscriptionEntry struct {
 	ServiceName string `json:"service_name" validator:"required,alphanum"`
 	Price       int    `json:"price" validator:"required,numeric"`
 	UserID      string `json:"user_id" validator:"required,uuid"`
@@ -18,4 +20,42 @@ type DummySubscriptionEntry struct {
 	EndDate     string `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
 }
 
-// TODO - создать разные Data Transfer Objects
+// Структура для фильтрации данных для remove
+type FilterRemoveSubscriptionEntry struct {
+	ServiceName string `json:"service_name" validator:"omitempty,alphanum"`
+	UserID      string `json:"user_id" validator:"required,uuid"`
+}
+
+// Структура для фильтрации данных для read
+type FilterReadSubscriptionEntry struct {
+	ServiceName string     `json:"service_name,omitempty" validator:"omitempty,alphanum"`
+	Price       int        `json:"price,omitempty" validator:"omitempty,numeric"`
+	UserID      string     `json:"user_id" validator:"required,uuid"`
+	StartDate   time.Time  `json:"start_date,omitempty" validator:"omitempty,datetime=01-2006"`
+	EndDate     *time.Time `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
+}
+
+// Структура для фильтрации данных для update
+type FilterUpdateSubscriptionEntry struct {
+	ServiceName string     `json:"service_name" validator:"required,alphanum"`
+	Price       int        `json:"price" validator:"omitempty,numeric"`
+	UserID      string     `json:"user_id" validator:"required,uuid"`
+	StartDate   time.Time  `json:"start_date" validator:"omitempty,datetime=01-2006"`
+	EndDate     *time.Time `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
+}
+
+type DummyFilterUpdateSubscriptionEntry struct {
+	ServiceName string `json:"service_name" validator:"required,alphanum"`
+	Price       int    `json:"price" validator:"omitempty,numeric"`
+	UserID      string `json:"user_id" validator:"required,uuid"`
+	StartDate   string `json:"start_date" validator:"omitempty,datetime=01-2006"`
+	EndDate     string `json:"end_date,omitempty" validator:"omitempty,datetime=01-2006"`
+}
+
+type ListSubscriptionEntrys struct {
+	ServiceName string     `json:"service_name"`
+	Price       int        `json:"price"`
+	UserID      string     `json:"user_id"`
+	StartDate   time.Time  `json:"start_date"`
+	EndDate     *time.Time `json:"end_date,omitempty"`
+}
