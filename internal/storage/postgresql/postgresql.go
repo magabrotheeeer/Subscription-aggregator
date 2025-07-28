2e0697bfaa565b9481012a73710c84c7073f58b3
Magomed <akhilgov_magomed@inbox.ru>
2025-07-28 16:18:13 +0300

    теперь функция create возвращает количество записей в бд; добавлена функция, которая обращается к бд, для подсчета суммы по пользователю за период

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -43,30 +43,36 @@ func New(storageConnectionString string) (*Storage, error) {
 }
 
 func (s *Storage) CreateSubscriptionEntry(ctx context.Context, entry subs.CreaterSubscriptionEntry) (int, error) {
-
-	const op = "storage.postgresql.CreateSubscriptionEntry"
-	var result int
-	err := s.db.QueryRow(ctx, `
-		INSERT INTO subscriptions (
-			service_name,
-			price,
-			user_id,
-			start_date,
-			end_date
-		) VALUES ($1, $2, $3, $4, $5)
-		RETURNING id`,
-		entry.ServiceName,
-		entry.Price,
-		entry.UserID,
-		entry.StartDate,
-		entry.EndDate).Scan(&result)
-	if err != nil {
-		return 0, fmt.Errorf("%s: %w", op, err)
-	}
-	return result, nil
-
+    const op = "storage.postgresql.CreateSubscriptionEntry"
+    
+    _, err := s.db.Exec(ctx, `
+        INSERT INTO subscriptions (
+            service_name,
+            price,
+            user_id,
+            start_date,
+            end_date
+        ) VALUES ($1, $2, $3, $4, $5)`,
+        entry.ServiceName,
+        entry.Price,
+        entry.UserID,
+        entry.StartDate,
+        entry.EndDate)
+    
+    if err != nil {
+        return 0, fmt.Errorf("%s: %w", op, err)
+    }
+    
+    var res int
+    err = s.db.QueryRow(ctx, "SELECT COUNT(*) FROM subscriptions").Scan(&res)
+    if err != nil {
+        return 0, fmt.Errorf("%s: %w", op, err)
+    }
+    
+    return res, nil
 }
 
+// исходя из задания не понял, могу ли я удалять пользователей
 func (s *Storage) RemoveSubscriptionEntryByUserID(ctx context.Context, entry subs.FilterRemoverSubscriptionEntry) (int64, error) {
 
 	const op = "storage.postgresql.DeleteSubscriptionEntryByUserID"
@@ -81,6 +87,7 @@ func (s *Storage) RemoveSubscriptionEntryByUserID(ctx context.Context, entry sub
 	return result, nil
 }
 
+// исходя из задания не понял, могу ли я удалять пользователей
 func (s *Storage) RemoveSubscriptionEntryByServiceName(ctx context.Context, entry subs.FilterRemoverSubscriptionEntry) (int64, error) {
 
 	const op = "storage.postgresql.DeleteSubscriptionEntryByServiceName"
@@ -165,4 +172,28 @@ func (s *Storage) ListSubscriptionEntrys(ctx context.Context) ([]*subs.ListSubsc
 	}
 	return result, nil
 
+}
+
+func (s *Storage) CountSumSubscriptionEntrys(ctx context.Context, entry subs.CounterSumSubscriptionEntrys) (float64, error) {
+	const op = "storage.postgresql.CountSumSubscriptionEntrys"
+
+	var res *float64
+
+	err := s.db.QueryRow(ctx, `
+		SELECT SUM(price)
+		FROM subscriptions
+		WHERE user_id = $1
+			AND service_name = $2
+			AND start_date <= $3
+			AND (end_date IS NULL OR end_date >= $4)`,
+		entry.UserID, entry.ServiceName, entry.EndDate, entry.StartDate).Scan(&res)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	if res == nil {
+		return 0, nil 
+	}
+
+	return *res, nil
 }
