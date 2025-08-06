de859fc06fdfda6e51714969a95564bb13379ff9
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-08-06 21:50:35 +0300

    добавил функции для хэширования и проверку хэша по полученному паролю

--- internal/http-server/auth/auth.go
+++ internal/http-server/auth/auth.go
@@ -1,3 +1,26 @@
 package auth
 
-// TODO хешироние пароля при регистрации
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GetHash(password string) (string, error) {
+	const op = "auth.gethash"
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+	return string(hashedPassword), nil
+}
+
+func CompareHash(originalHash, externalPassword string) error {
+	const op = "auth.comparehash"
+	err := bcrypt.CompareHashAndPassword([]byte(originalHash), []byte(externalPassword))
+	if err == nil {
+		return nil
+	} else {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+}
