cbaa6646d93bf979a1112612390b1f7aa2c00c47
Magomed <akhilgov_magomed@inbox.ru>
2025-08-07 14:06:04 +0300

    добавлены структуры для работы с обновленным конфигом; добавлен метод для форматированной печати конфига

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -11,13 +12,30 @@ import (
 type Config struct {
 	Env                     string `yaml:"env"`
 	StorageConnectionString string `yaml:"storage_connection_string"`
+	RedisConnection         `yaml:"redis_connection"`
 	HTTPServer              `yaml:"http_server"`
+	JWTToken                `yaml:"jwttoken"`
 }
 
 type HTTPServer struct {
 	Address     string        `yaml:"address"`
 	Timeout     time.Duration `yaml:"timeout"`
 	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+type RedisConnection struct {
+	Addr        string        `yaml:"addr"`
+	Password    string        `yaml:"password"`
+	User        string        `yaml:"user"`
+	DB          int           `yaml:"db"`
+	MaxRetries  int           `yaml:"max_retries"`
+	DialTimeout time.Duration `yaml:"dial_timeout"`
+	Timeout     time.Duration `yaml:"timeout"`
+}
+
+type JWTToken struct {
+	JWTSecretKey string        `yaml:"jwt_secret_key"`
+	TokenTTL          time.Duration `yaml:"token_ttl"`
 }
 
 func MustLoad() *Config {
@@ -36,3 +54,41 @@ func MustLoad() *Config {
 	return &cfg
 
 }
+
+func (c *Config) String() string {
+    return fmt.Sprintf(
+        "Env: %s\n"+
+            "StorageConnectionString: %s\n"+
+            "RedisConnection:\n"+
+            "  Addr: %s\n"+
+            "  Password: %s\n"+
+            "  User: %s\n"+
+            "  DB: %d\n"+
+            "  MaxRetries: %d\n"+
+            "  DialTimeout: %s\n"+
+            "  Timeout: %s\n"+
+            "HTTPServer:\n"+
+            "  Address: %s\n"+
+            "  Timeout: %s\n"+
+            "  IdleTimeout: %s\n"+
+            "JWTToken:\n"+
+            "  JWTSecretKey: %s\n"+
+            "  TokenTTL: %s\n",
+        c.Env,
+        c.StorageConnectionString,
+        c.RedisConnection.Addr,
+        c.RedisConnection.Password,
+        c.RedisConnection.User,
+        c.RedisConnection.DB,
+        c.RedisConnection.MaxRetries,
+        c.RedisConnection.DialTimeout,
+        c.RedisConnection.Timeout,
+        c.HTTPServer.Address,
+        c.HTTPServer.Timeout,
+        c.HTTPServer.IdleTimeout,
+        c.JWTToken.JWTSecretKey,
+        c.JWTToken.TokenTTL,
+    )
+}
+
+
