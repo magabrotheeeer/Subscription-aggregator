76c965d01363e76a40bb38d65b961b7eeb34fffc
Magomed <akhilgov_magomed@inbox.ru>
2025-08-08 16:22:32 +0300

    функии для работы с датами

---
+++ internal/util/util.go
@@ -0,0 +1,23 @@
+package util
+
+import "time"
+
+func MonthsBetween(from, to time.Time) int {
+	y1, m1 := from.Year(), from.Month()
+	y2, m2 := to.Year(), to.Month()
+	return (y2-y1)*12 + int(m2-m1) + 1
+}
+
+func MaxDate(a, b time.Time) time.Time {
+	if a.After(b) {
+		return a
+	}
+	return b
+}
+
+func MinDate(a, b time.Time) time.Time {
+	if a.Before(b) {
+		return a
+	}
+	return b
+}
