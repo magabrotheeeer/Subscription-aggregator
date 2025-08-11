1998bc123cd557aed39f912cc8a2fb4f734dbe18
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-08-12 00:56:52 +0300

    добавил комментарии

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,4 +1,4 @@
-// Package cache предоставляет функции и структуру для работы с Redis
+// Package cache предоставляет функционал для работы с кэшированием данных в Redis.
 package cache
 
 import (
@@ -11,14 +11,18 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// Cache предоставляет подключение к бд
+// Cache представляет собой обёртку над клиентом Redis.
+// Содержит подключение к базе данных Redis.
 type Cache struct {
 	Db *redis.Client
 }
 
-// InitServer создает объект cache с настройками из config/config.yaml
+// InitServer инициализирует подключение к Redis и возвращает структуру Cache.
+// Настройки подключения берутся из переданного конфигурационного объекта cfg.
+// При невозможности подключения возвращается ошибка.
 func InitServer(ctx context.Context, cfg config.RedisConnection) (*Cache, error) {
-	const op = "cache.Initserver"
+	const op = "cache.InitServer"
+
 	db := redis.NewClient(&redis.Options{
 		Addr:         cfg.AddressRedis,
 		Password:     cfg.Password,
@@ -33,12 +37,16 @@ func InitServer(ctx context.Context, cfg config.RedisConnection) (*Cache, error)
 	if err := db.Ping(ctx).Err(); err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
+
 	return &Cache{Db: db}, nil
 }
 
-// Get проверяет наличие значения по ключу key и записывает это же значение результат в result
+// Get получает значение из Redis по ключу key и записывает его в result.
+// Возвращает true, если значение найдено, и false — если ключ отсутствует.
+// В случае ошибки возвращает её.
 func (c *Cache) Get(key string, result any) (bool, error) {
 	const op = "cache.Get"
+
 	val, err := c.Db.Get(context.Background(), key).Result()
 	if err == redis.Nil {
 		return false, nil
@@ -46,15 +54,17 @@ func (c *Cache) Get(key string, result any) (bool, error) {
 	if err != nil {
 		return false, fmt.Errorf("%s: %w", op, err)
 	}
+
 	err = json.Unmarshal([]byte(val), result)
 	if err != nil {
 		return false, fmt.Errorf("%s: %w", op, err)
 	}
+
 	return true, nil
 }
 
-// Set добавляет новое значение value по ключу key
-// expiration - время хранение в redis
+// Set записывает значение value по ключу key в Redis.
+// expiration задаёт время жизни ключа. При ошибке возвращает её.
 func (c *Cache) Set(key string, value any, expiration time.Duration) error {
 	jsonData, err := json.Marshal(value)
 	if err != nil {
@@ -63,7 +73,8 @@ func (c *Cache) Set(key string, value any, expiration time.Duration) error {
 	return c.Db.Set(context.Background(), key, jsonData, expiration).Err()
 }
 
-// Invalidate удаляет значение по ключу key
+// Invalidate удаляет значение из Redis по указанному ключу key.
+// Возвращает ошибку, если операция удаления не удалась.
 func (c *Cache) Invalidate(key string) error {
 	return c.Db.Del(context.Background(), key).Err()
 }
