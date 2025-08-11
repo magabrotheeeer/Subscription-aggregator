85ac7e230a1ad00f78c53b8b8c3699413f826ab2
Magomed <akhilgov_magomed@inbox.ru>
2025-08-11 14:58:05 +0300

    добавил тесты

---
+++ internal/lib/sl/sl_test.go
@@ -0,0 +1,25 @@
+package sl_test
+
+import (
+    "errors"
+    "log/slog"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+
+    "github.com/magabrotheeeer/subscription-aggregator/internal/lib/sl"
+)
+
+func TestErr_ReturnsCorrectAttr(t *testing.T) {
+    err := errors.New("something went wrong")
+    attr := sl.Err(err)
+
+    assert.Equal(t, "error", attr.Key)
+    assert.Equal(t, slog.StringValue("something went wrong"), attr.Value)
+}
+
+func TestErr_NilError(t *testing.T) {
+    assert.Panics(t, func() {
+        _ = sl.Err(nil)
+    })
+}
