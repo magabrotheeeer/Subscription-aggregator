81e00de1f0b30194f53f82e6e3919789bae99ffd
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-08-18 18:42:10 +0300

    fix

--- cmd/subscription-aggregator/main.go
+++ cmd/subscription-aggregator/main.go
@@ -18,8 +18,14 @@ import (
 	"github.com/magabrotheeeer/subscription-aggregator/internal/cache"
 	"github.com/magabrotheeeer/subscription-aggregator/internal/config"
 	"github.com/magabrotheeeer/subscription-aggregator/internal/grpc/client"
-	registerHandler "github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/auth/register"
-	createHandler "github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/subscription/create"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/auth/login"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/auth/register"
+	countsum "github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/subscription/count_sum"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/subscription/create"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/subscription/list"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/subscription/read"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/subscription/remove"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/http/handlers/subscription/update"
 	"github.com/magabrotheeeer/subscription-aggregator/internal/http/middlewarectx"
 	"github.com/magabrotheeeer/subscription-aggregator/internal/lib/jwt"
 	"github.com/magabrotheeeer/subscription-aggregator/internal/services"
@@ -53,7 +59,6 @@ func main() {
 	}
 	defer authClient.Close()
 
-	// JWT Maker
 	jwtMaker := jwt.NewJWTMaker(config.JWTSecretKey, config.TokenTTL)
 
 	subscriptionService := services.NewSubscriptionService(storage, cache, logger)
@@ -65,13 +70,21 @@ func main() {
 		middleware.Recoverer,
 		middleware.URLFormat,
 	)
-	// todo
+
 	router.Route("/api/v1", func(r chi.Router) {
-		r.Post("/register", registerHandler.New(logger, authClient).ServeHTTP)
+		r.Post("/register", register.New(logger, authClient).ServeHTTP)
+		r.Post("/login", login.New(logger, authClient).ServeHTTP)
 
 		r.Group(func(r chi.Router) {
 			r.Use(middlewarectx.JWTMiddleware(jwtMaker, logger))
-			r.Post("/subscriptions", createHandler.New(logger, subscriptionService).ServeHTTP)
+
+			r.Post("/subscriptions", create.New(logger, subscriptionService).ServeHTTP)
+			r.Get("/subscriptions/{id}", read.New(logger, subscriptionService).ServeHTTP)
+			r.Delete("/subscriptions/{id}", remove.New(logger, subscriptionService).ServeHTTP)
+			r.Put("/subscriptions/{id}", update.New(logger, subscriptionService).ServeHTTP)
+
+			r.Get("/subscriptions/list", list.New(logger, subscriptionService).ServeHTTP)
+			r.Post("/subscriptions/sum", countsum.New(logger, subscriptionService).ServeHTTP)
 		})
 	})
 
@@ -95,7 +108,7 @@ func main() {
 	}()
 
 	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 
 	select {
 	case err := <-serverError:
