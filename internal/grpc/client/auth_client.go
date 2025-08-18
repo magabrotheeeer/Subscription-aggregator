ac8d6a293304eb67d30d0ad828c8523803e6794b
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-08-18 18:42:53 +0300

    fix

--- internal/grpc/client/auth_client.go
+++ internal/grpc/client/auth_client.go
@@ -2,9 +2,9 @@ package client
 
 import (
 	"context"
-	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	authpb "github.com/magabrotheeeer/subscription-aggregator/internal/grpc/proto"
 )
@@ -15,10 +15,7 @@ type AuthClient struct {
 }
 
 func NewAuthClient(addr string) (*AuthClient, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	conn, err := grpc.DialContext(ctx, addr, grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
