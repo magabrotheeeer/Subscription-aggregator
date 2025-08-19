27524d9495865c31a857596be78a413c9b6bd534
Magomed <akhilgov_magomed@inbox.ru>
2025-08-19 11:19:06 +0300

    функция для запуска миграций

---
+++ internal/migrations/run.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"database/sql"
+
+	"github.com/golang-migrate/migrate/v4"
+	pgxv5 "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+)
+
+func Run(db *sql.DB, path string) error {
+	driver, err := pgxv5.WithInstance(db, &pgxv5.Config{})
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://"+path,
+		"pgx_v5",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
