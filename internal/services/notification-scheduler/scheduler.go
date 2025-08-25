6e63b9117d3aeff4473abf0a73b68b1056e4faa5
Magomed <akhilgov_magomed@inbox.ru>
2025-08-25 17:38:47 +0300

    add: добавлена отдельная функция, чтобы поиск устаревших подписок запускался сразу после запуска сервиса, а потом каждые 12 часов

--- internal/services/notification-scheduler/scheduler.go
+++ internal/services/notification-scheduler/scheduler.go
@@ -29,21 +29,32 @@ func NewSchedulerService(repo SubscriptionRepository, log *slog.Logger) *Schedul
 }
 
 func (s *SchedulerService) FindExpiringSubscriptions(ctx context.Context, channel *amqp.Channel) {
+	s.runFindExpiringSubscriptions(ctx, channel)
+
 	ticker := time.NewTicker(12 * time.Hour)
 	defer ticker.Stop()
 
 	for range ticker.C {
-		s.log.Info("starting service to find expiring subscriptions")
-		entriesInfo, err := s.repo.FindSubscriptionExpiringTomorrow(ctx)
+		s.runFindExpiringSubscriptions(ctx, channel)
+	}
+}
+
+func (s *SchedulerService) runFindExpiringSubscriptions(ctx context.Context, channel *amqp.Channel) {
+	s.log.Info("starting service to find expiring subscriptions")
+	entriesInfo, err := s.repo.FindSubscriptionExpiringTomorrow(ctx)
+	if err != nil {
+		s.log.Error("failed to find entries", sl.Err(err))
+		return
+	}
+	if len(entriesInfo) == 0 {
+		s.log.Info("no expiring subscriptions found")
+		return
+	}
+	s.log.Info("found expiring subscriptions", "count", len(entriesInfo))
+	for _, entryInfo := range entriesInfo {
+		err = rabbitmq.PublishMessage(channel, "notifications", "upcoming", entryInfo)
 		if err != nil {
-			s.log.Error("failed to find entries", sl.Err(err))
-		}
-		for _, entryInfo := range entriesInfo {
-			err = rabbitmq.PublishMessage(channel, "notifications", "upcoming", entryInfo)
-			if err != nil {
-				s.log.Error("failed to publish message", sl.Err(err))
-			}
+			s.log.Error("failed to publish message", sl.Err(err))
 		}
 	}
-
 }
