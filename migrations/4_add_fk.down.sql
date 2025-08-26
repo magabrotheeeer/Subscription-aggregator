eb2757d3c0429cd75f943abfe0c63e0adac3a666
Magomed <akhilgov_magomed@inbox.ru>
2025-08-26 19:22:42 +0300

    add: внешние ключе по полям username и user_uid

--- migrations/4_add_fk.down.sql
+++ migrations/4_add_fk.down.sql
@@ -1,1 +1,5 @@
-ALTER TABLE subscriptions DROP CONSTRAINT IF EXISTS fk_subscriptions_user;
+ALTER TABLE subscriptions
+DROP CONSTRAINT IF EXISTS fk_subscriptions_user_username;
+
+ALTER TABLE subscriptions
+DROP CONSTRAINT IF EXISTS fk_subscriptions_user_uid;
