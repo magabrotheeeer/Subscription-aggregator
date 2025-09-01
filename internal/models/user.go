4fdc2dca1280869bf5865c3b57efcc99d75f2604
Magomed Akhilgov <akhilgov_magomed@inbox.ru>
2025-09-01 20:48:26 +0300

    add: добавлены поля в доменную модель с датой окончания пробной подписки и датой окончания оплаченной подписки

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,11 +3,15 @@
 // Структура используется в бизнес‑логике и при работе с хранилищем.
 package models
 
+import "time"
+
 // User представляет зарегистрированного пользователя системы.
 type User struct {
-	UUID         string // Уникальный идентификатор пользователя
-	Email        string // Электронная почта
-	Username     string // Имя пользователя (уникальное)
-	PasswordHash string // Хэш пароля пользователя
-	Role         string // Роль пользователя, admin или user
+	UUID               string    // Уникальный идентификатор пользователя
+	Email              string    // Электронная почта
+	Username           string    // Имя пользователя (уникальное)
+	PasswordHash       string    // Хэш пароля пользователя
+	Role               string    // Роль пользователя, admin или user
+	TrialEndDate       time.Time // Дата истечения пробного периода
+	SubscriptionExpire time.Time // Дата истечения оплаченной подписки на сервис
 }
