707e019a6684ab7d6fdea9206a428f82816bec8f
Magomed Akhilgov <akhilgov_magomed@inbox.ru>
2025-09-01 21:01:44 +0300

    fix: убрал неиспользуемую функцию

--- internal/services/scheduler/scheduler.go
+++ internal/services/scheduler/scheduler.go
@@ -13,7 +13,6 @@ import (
 
 type SubscriptionRepository interface {
 	FindSubscriptionExpiringTomorrow(ctx context.Context) ([]*models.EntryInfo, error)
-	FindSubscriptionsDueToday(ctx context.Context) ([]*models.Entry, error)
 }
 
 type SchedulerService struct {
@@ -54,37 +53,6 @@ func (s *SchedulerService) runFindExpiringSubscriptionsDueTomorrow(ctx context.C
 	s.log.Info("found expiring subscriptions", "count", len(entriesInfo))
 	for _, entryInfo := range entriesInfo {
 		err = rabbitmq.PublishMessage(channel, "notifications", "upcoming", entryInfo)
-		if err != nil {
-			s.log.Error("failed to publish message", sl.Err(err))
-		}
-	}
-}
-
-func (s *SchedulerService) FindExpiringSubscriptionsDueToday(ctx context.Context, channel *amqp.Channel) {
-	s.runFindExpiringSubscriptionsDueToday(ctx, channel)
-
-	ticker := time.NewTicker(24 * time.Hour)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		s.runFindExpiringSubscriptionsDueToday(ctx, channel)
-	}
-}
-
-func (s *SchedulerService) runFindExpiringSubscriptionsDueToday(ctx context.Context, channel *amqp.Channel) {
-	s.log.Info("starting service to find expiring subscriptions due today")
-	entriesInfo, err := s.repo.FindSubscriptionsDueToday(ctx)
-	if err != nil {
-		s.log.Error("failed to find entries", sl.Err(err))
-		return
-	}
-	if len(entriesInfo) == 0 {
-		s.log.Info("no subscriptions for pay found")
-		return
-	}
-	s.log.Info("found subscriptions for pay", "count", len(entriesInfo))
-	for _, entryInfo := range entriesInfo {
-		err = rabbitmq.PublishMessage(channel, "payment", "due", entryInfo)
 		if err != nil {
 			s.log.Error("failed to publish message", sl.Err(err))
 		}
