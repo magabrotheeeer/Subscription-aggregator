e2d69e01151e688ec51f6879cdf8f824bbb0d2e4
Magomed <akhilgov_magomed@inbox.ru>
2025-09-02 15:36:09 +0300

    add: эндпоинт для отправки данных о пользователе в юкассу

--- internal/http/handlers/payment/paymentcreate/create.go
+++ internal/http/handlers/payment/paymentcreate/create.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"net/http"
 
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
 	"github.com/go-playground/validator"
 	"github.com/magabrotheeeer/subscription-aggregator/internal/http/middlewarectx"
@@ -23,46 +22,36 @@ type CreatePaymentMethodRequestApp struct {
 }
 
 type ProviderService interface {
-	CreatePaymentMethod(req paymentprovider.CreatePaymentMethodRequest) (*paymentprovider.CreatePaymentMethodResponse, error)
-}
-
-type AppService interface {
-	//TODO: реализовать функции для работа с бизнес-слоем
-	CreatePaymentMethod(paymentResponse paymentprovider.CreatePaymentMethodResponse)
+	CreatePayment(reqParams paymentprovider.CreatePaymentRequest) (*paymentprovider.CreatePaymentResponse, error)
 }
 
 type Handler struct {
-	log             *slog.Logger    // Логгер для записи информации и ошибок
-	providerService ProviderService // Сервис для работы с провайдером
-	appService      AppService
+	log             *slog.Logger        // Логгер для записи информации и ошибок
+	providerService ProviderService     // Сервис для работы с провайдером
 	validate        *validator.Validate // Валидатор структуры входящих данных
 }
 
-func New(log *slog.Logger, providerService ProviderService, appService AppService) *Handler {
+func New(log *slog.Logger, providerService ProviderService) *Handler {
 	return &Handler{
 		log:             log,
 		providerService: providerService,
-		appService:      appService,
 		validate:        validator.New(),
 	}
 }
 
+// ServeHTTP возвращает клиенту confirmation_token, полученный от ЮКАССа
+// Точка приема платежа от frontend
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	const op = "handlers.payment.create"
+	log := h.log.With(slog.String("op", op))
 
-	log := h.log.With(
-		slog.String("op", op),
-		slog.String("request_id", middleware.GetReqID(r.Context())),
-	)
-
-	var req paymentprovider.CreatePaymentMethodRequest
+	var req CreatePaymentMethodRequestApp
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		log.Error("failed to decode request", sl.Err(err))
 		w.WriteHeader(http.StatusBadRequest)
 		render.JSON(w, r, response.Error("invalid request body"))
 		return
 	}
-	log.Info("request body decoded", slog.Any("request", req))
 
 	if err := h.validate.Struct(req); err != nil {
 		log.Error("validation failed", sl.Err(err))
@@ -70,21 +59,33 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, r, response.ValidationError(err.(validator.ValidationErrors)))
 		return
 	}
-	log.Info("all fields are validated")
 
-	useruid, ok := r.Context().Value(middlewarectx.UserUID).(string)
-	if !ok || useruid == "" {
-		log.Error("username not found in context")
+	userUID, ok := r.Context().Value(middlewarectx.UserUID).(string)
+	if !ok || userUID == "" {
+		log.Error("user UID not found in context")
 		w.WriteHeader(http.StatusUnauthorized)
 		render.JSON(w, r, response.Error("unauthorized"))
 		return
 	}
-	responseFromProvider, err := h.providerService.CreatePaymentMethod(req)
-	if err != nil {
-		log.Error("failed to get response from provider", sl.Err(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		render.JSON(w, r, response.Error("failed to get response from provider"))
-	}
-	h.appService.CreatePaymentMethod(*responseFromProvider)
 
+	paymentReq := paymentprovider.CreatePaymentRequest{
+		PaymentToken: req.PaymentMethodToken,
+		Amount: paymentprovider.Amount{
+			Value:    "200.00",
+			Currency: "RUB",
+		},
+		Metadata: map[string]string{
+			"user_uid": userUID,
+		},
+	}
+
+	paymentResp, err := h.providerService.CreatePayment(paymentReq)
+	if err != nil {
+		log.Error("failed to create payment method from provider", sl.Err(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		render.JSON(w, r, response.Error("payment provider error"))
+		return
+	}
+
+	render.JSON(w, r, paymentResp)
 }
