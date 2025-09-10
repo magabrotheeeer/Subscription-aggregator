25729c9931ded00a108fe0299bc4247505b85428
Magomed <akhilgov_magomed@inbox.ru>
2025-09-10 15:54:55 +0300

    style: поправил ошибки линтера

--- internal/models/subscription.go
+++ internal/models/subscription.go
@@ -24,13 +24,14 @@ type Entry struct {
 // прежде чем конвертировать их в SubscriptionEntry.
 // Даты приходят в виде строк, чтобы их можно было валидировать и парсить вручную.
 type DummyEntry struct {
-	ServiceName   string `json:"service_name" validate:"required"`         // Название сервиса
-	Price         int    `json:"price" validate:"required,gt=0"`           // Цена (>0)
-	StartDate     string `json:"start_date" validate:"required"`           // Дата начала в формате 01-2006
+	ServiceName   string `json:"service_name" validate:"required"`        // Название сервиса
+	Price         int    `json:"price" validate:"required,gt=0"`          // Цена (>0)
+	StartDate     string `json:"start_date" validate:"required"`          // Дата начала в формате 01-2006
 	CounterMonths int    `json:"counter_months" validate:"required,gt=0"` // Количество месяцев
-	IsActive      bool   `json:"is_active" validate:"required"`
+	IsActive      bool   `json:"is_active"`
 }
 
+// EntryInfo содержит информацию о подписке для уведомлений.
 type EntryInfo struct {
 	Email       string
 	Username    string
