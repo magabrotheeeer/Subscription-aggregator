3a885185abd74215c99cf02d224bd9195c2b0c4f
Magomed <akhilgov_magomed@inbox.ru>
2025-09-10 15:54:10 +0300

    fix: поправил типы полей

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,12 +7,12 @@ import "time"
 
 // User представляет зарегистрированного пользователя системы.
 type User struct {
-	UUID               string    // Уникальный идентификатор пользователя
-	Email              string    // Электронная почта
-	Username           string    // Имя пользователя (уникальное)
-	PasswordHash       string    // Хэш пароля пользователя
-	Role               string    // Роль пользователя, admin или user
-	TrialEndDate       time.Time // Дата истечения пробного периода
-	SubscriptionExpire time.Time // Дата истечения оплаченной подписки на сервис
+	UUID               string     // Уникальный идентификатор пользователя
+	Email              string     // Электронная почта
+	Username           string     // Имя пользователя (уникальное)
+	PasswordHash       string     // Хэш пароля пользователя
+	Role               string     // Роль пользователя, admin или user
+	TrialEndDate       *time.Time // Дата истечения пробного периода
+	SubscriptionExpire *time.Time // Дата истечения оплаченной подписки на сервис
 	SubscriptionStatus string
 }
