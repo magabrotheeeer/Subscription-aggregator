0c308eb04b13b163f0f9cf307b261558a1855d27
Magomed <akhilgov_magomed@inbox.ru>
2025-09-23 17:38:45 +0300

    add: проверка работоспособности компонентов сервиса

--- internal/http/handlers/subscription/health/health.go
+++ internal/http/handlers/subscription/health/health.go
@@ -5,22 +5,31 @@ import (
 	"net/http"
 
 	"github.com/go-chi/render"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/cache"
 	"github.com/magabrotheeeer/subscription-aggregator/internal/http/response"
+	"github.com/magabrotheeeer/subscription-aggregator/internal/storage"
+	"github.com/streadway/amqp"
 )
 
 type Handler struct {
-	log *slog.Logger
+	log     *slog.Logger
+	storage *storage.Storage
+	rabbit  *amqp.Connection
+	cache   *cache.Cache
 }
 
-func New(log *slog.Logger) *Handler {
+func New(log *slog.Logger, storage *storage.Storage, rabbit *amqp.Connection, cache *cache.Cache) *Handler {
 	return &Handler{
 		log: log,
+		storage: storage,
+		rabbit: rabbit,
+		cache: cache,
 	}
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	const op = "handlers.subscription.health"
-	w.WriteHeader(http.StatusOK)	
+	w.WriteHeader(http.StatusOK)
 	render.JSON(w, r, response.OKWithData(map[string]any{
 		"status": "ok",
 	}))
